0e84b30b036322c6f038e635cafc9270ce4b3e87
lcamus <laurent.f.camus@gmail.com>
2017-04-10 00:19:04 +0200

    .

--- global.R
+++ global.R
@@ -21,9 +21,67 @@ for (i in h) {
   edges <- rbind(edges,data.frame(from=0, to=id,label=NA,title=NA))
 }
 
+h <- homepage %>% html_nodes("#container > div > div")
+for (i in 1:length(h)) {
+  nodes <- rbind(nodes,
+                 data.frame(
+                   id=sapply(seq_along(h[i] %>% html_nodes("a")),function(x){nrow(nodes)+x}),
+                   label=sapply(h[i] %>% html_nodes("a") %>% html_text(),function(x){x}),
+                   group="ext",
+                   value=1,
+                   title="title",
+                   url=sapply(h[i] %>% html_nodes("a") %>% html_attrs(),function(x){x}))
+                 )
+  edges <- rbind(edges,
+                 data.frame(
+                   from=i,
+                   to=sapply(seq_along(h[i] %>% html_nodes("a")),function(x){nrow(nodes)-x+1}),
+                   label=NA,
+                   title=NA)
+  )
+}
+
+# Insights
+h <- homepage %>% html_nodes("body > div.main-container.main > div > div > div:nth-child(3) a")
+nodes <- rbind(nodes,
+               data.frame(
+                 id=nrow(nodes)+1,
+                 label="Insights",
+                 group="ext",
+                 value=1,
+                 title="Insights",
+                 url=as.character(h %>% html_attrs()))
+)
+edges <- rbind(edges,
+               data.frame(
+                 from=0,
+                 to=nrow(nodes),
+                 label=NA,
+                 title=NA)
+)
+
+#Bank's corner
+h <- homepage %>% html_nodes("body > div.main-container.main > div > div > div:nth-child(4) a")
+nodes <- rbind(nodes,
+               data.frame(
+                 id=nrow(nodes)+1,
+                 label="Bank's corner",
+                 group="ext",
+                 value=1,
+                 title="Bank's corner",
+                 url=as.character(h %>% html_attrs()))
+)
+edges <- rbind(edges,
+               data.frame(
+                 from=0,
+                 to=nrow(nodes),
+                 label=NA,
+                 title=NA)
+)
+
 visNetwork(nodes, edges)  
 
-sub_indicators <- homepage %>% html_nodes("#container > div > div li") %>% html_text()
-sub_indicators_lib <- setNames(sapply(sub_indicators,function(x) gsub("^[ ,\t,\n]+|[ ,\t,\n]+$","",x)),NULL)
-sub_indicators_ref <- sub("&lg=.*$","",
-                          homepage %>% html_nodes("#container > div > div li a") %>% html_attr("href"))
+# sub_indicators <- homepage %>% html_nodes("#container > div > div li") %>% html_text()
+# sub_indicators_lib <- setNames(sapply(sub_indicators,function(x) gsub("^[ ,\t,\n]+|[ ,\t,\n]+$","",x)),NULL)
+# sub_indicators_ref <- sub("&lg=.*$","",
+#                           homepage %>% html_nodes("#container > div > div li a") %>% html_attr("href"))
