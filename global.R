a37a25438d6a9000b4f322f8faff6836326030d3
lcamus <laurent.f.camus@gmail.com>
2017-04-16 10:22:56 +0200

    .

--- global.R
+++ global.R
@@ -8,16 +8,17 @@ url_root <- "https://www.euro-area-statistics.org"
 nodes <- data.frame()
 edges <- data.frame()
 
-nodes <- data.frame(id=0,label="homepage",group="ext",value=1,title="homepage",url=url_root)
-edges <- data.frame(from=0, to=0, label=NA, title=NA)
+nodes <- data.frame(id=0,label="homepage",group="global",value=1,title="home",url=url_root)
+edges <- data.frame(from=0, to=0, label=NA, title="homepage")
+edges <- data.frame(from=0, to=0, label=NA, title="langue")
 
-homepage <- read_html(url)
+homepage <- read_html(url_root)
 
 # traitement du bloc des statistiques par domaine
 h <- homepage %>% html_nodes("#container > div > div > h2") %>% html_text()
 for (i in h) {
   id <- max(nodes$id)+1
-  nodes <- rbind(nodes,data.frame(id,label=i,group="int",value=1,title=i,url=NA))
+  nodes <- rbind(nodes,data.frame(id,label=i,group="Indicators",value=1,title=i,url=NA))
   edges <- rbind(edges,data.frame(from=0, to=id,label=NA,title=NA))
 }
 
@@ -27,7 +28,7 @@ for (i in 1:length(h)) {
                  data.frame(
                    id=sapply(seq_along(h[i] %>% html_nodes("a")),function(x){nrow(nodes)+x}),
                    label=sapply(h[i] %>% html_nodes("a") %>% html_text(),function(x){x}),
-                   group="ext",
+                   group="Indicators",
                    value=1,
                    title="title",
                    url=sapply(h[i] %>% html_nodes("a") %>% html_attrs(),function(x){x}))
@@ -43,14 +44,15 @@ for (i in 1:length(h)) {
 
 # Insights
 h <- homepage %>% html_nodes("body > div.main-container.main > div > div > div:nth-child(3) a")
+url_insights <- as.character(h %>% html_attrs())
 nodes <- rbind(nodes,
                data.frame(
                  id=nrow(nodes)+1,
                  label="Insights",
-                 group="ext",
+                 group="Insights",
                  value=1,
-                 title="Insights",
-                 url=as.character(h %>% html_attrs()))
+                 title=paste0(nrow(nodes)+1,"Insights"),
+                 url=url_insights)
 )
 edges <- rbind(edges,
                data.frame(
@@ -60,13 +62,26 @@ edges <- rbind(edges,
                  title=NA)
 )
 
+insights <- read_html(paste(url_root,url_insights,sep="/"))
+h <- insights %>% html_nodes("#ieas > aside > div.insights > ul li a") %>% html_attrs()
+nodes <- rbind(nodes,
+               data.frame(
+                 id=nrow(nodes)+1,
+                 label="Insights",
+                 group="Insights",
+                 value=1,
+                 title=paste0(nrow(nodes)+1,"Insights"),
+                 url=sapply(h,FUN=function(x){x["href"]})
+               )
+)
+
 #Bank's corner
 h <- homepage %>% html_nodes("body > div.main-container.main > div > div > div:nth-child(4) a")
 nodes <- rbind(nodes,
                data.frame(
                  id=nrow(nodes)+1,
                  label="Bank's corner",
-                 group="ext",
+                 group="Bank's corner",
                  value=1,
                  title="Bank's corner",
                  url=as.character(h %>% html_attrs()))
@@ -79,9 +94,8 @@ edges <- rbind(edges,
                  title=NA)
 )
 
-visNetwork(nodes, edges)  
 
-# sub_indicators <- homepage %>% html_nodes("#container > div > div li") %>% html_text()
-# sub_indicators_lib <- setNames(sapply(sub_indicators,function(x) gsub("^[ ,\t,\n]+|[ ,\t,\n]+$","",x)),NULL)
-# sub_indicators_ref <- sub("&lg=.*$","",
-#                           homepage %>% html_nodes("#container > div > div li a") %>% html_attr("href"))
+visNetwork(nodes, edges, height="700px", width="100%") %>%
+  visLegend()
+
+
