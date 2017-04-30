ea11b61be3a3b5bd6b1c07b0eeda18f603febb8c
lcamus <laurent.f.camus@gmail.com>
2017-04-30 23:34:35 +0200

    .

--- global.R
+++ global.R
@@ -11,12 +11,18 @@ base <- "https://utils.euro-area-statistics.org/admin/piwik/?module=API"
 idSite <- "1"
 format <- "CSV"
 token_auth <- as.character(read.table("private/token",header=F)[,])
+
 if (exists(fdata)) {
   load(fdata)
 } else {
-  d <<- list(UserCountry=list(getCountry=data.frame(), getContinent=data.frame(), getRegion=data.frame(), getCity=data.frame()))
+  d <<- list(UserCountry=list(getCountry=data.frame(), getContinent=data.frame(), getRegion=data.frame(), getCity=data.frame()),
+             UserLanguage=list(getLanguage=data.frame())
+  )
 }
-fieldstoremove <- c("metadata_logo","metadata_segment","metadata_logoWidth","metadata_logoHeight")  
+
+#hideCol <- list(UserCountry=list(getCountry=c("nb_visits_converted"), getContinent=c("nb_visits_converted"), getRegion=c("nb_visits_converted")))
+hideCol <- list()
+fieldstoremove <- c("metadata_logo","metadata_logoWidth","metadata_logoHeight", "nb_visits_converted")  
 
 url_root <- "https://www.euro-area-statistics.org"
 nodes <- data.frame()
--- src/data.R
+++ src/data.R
@@ -1,4 +1,12 @@
 
+collectData <- function(from, to) {
+  days <- seq(from=as.Date(from), to=as.Date(to),by='days' )
+  
+  for (object in names(d))
+    for (method in names(d[[object]]))
+      for (day in seq_along(days))
+        getData(day, object, method, update=F)
+}
 
 getAllData <- function(date, period) {
   m <- list()
@@ -23,12 +31,18 @@ getAllData <- function(date, period) {
   return(l)
 }
 
-getData <- function(date, object, method, hideColumns, period, filter_limit, updatemode) {
+getData <- function(date, object, method, hideColumns, period, filter_limit, updatemode, appendmode) {
   
   if (missing(filter_limit)) filter_limit <- "-1"
   if (missing(period)) period <- "day"
   if (missing(updatemode)) updatemode <- FALSE
-  u <- paste(base,
+  if (missing(appendmode)) appendmode <- FALSE
+  if (missing(hideColumns)) hideColumns <- paste(hideCol[[object]][[method]],collapse=",")
+
+  updatable <- nrow(d[[object]][[method]][d[[object]][[method]]$date==date,])>0
+  
+  if (!updatable |(updatable & updatemode)) {
+    u <- paste(base,
                paste("method=", object, ".", method, sep=""),
                paste0("idSite=",idSite),
                paste("date=", date, sep=""),
@@ -38,9 +52,6 @@ getData <- function(date, object, method, hideColumns, period, filter_limit, upd
                paste0("filter_limit=",filter_limit),
                paste0("hideColumns=",hideColumns),
                sep="&")
-  
-  updatable <- nrow(d[[object]][[method]][d[[object]][[method]]$date==date,])>0
-  if (!updatable |(updatable & updatemode)) {
     df <- cbind(date=date, read.csv(url(description=u, open="", blocking=TRUE, encoding="UTF-16",method="default"),header=T,sep=","))
     df <- df[,!colnames(df) %in% fieldstoremove]
   }
@@ -48,7 +59,10 @@ getData <- function(date, object, method, hideColumns, period, filter_limit, upd
     return
   
   if (!updatable) {
-    d[[object]][[method]] <<- rbind(d[[object]][[method]],df)
+    if (appendmode)
+      d[[object]][[method]] <<- rbind(d[[object]][[method]],df)
+    else
+      print(df)
   } else {
     d[[object]][[method]][d[[object]][[method]]$date==date,] <- df
   }
