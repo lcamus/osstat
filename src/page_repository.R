45e414efc0969cabde06dd114bda499cec085bdc
Laurent <laurent.f.camus@gmail.com>
2018-04-02 12:15:51 +0200

    .

--- src/page_repository.R
+++ src/page_repository.R
@@ -1,6 +1,19 @@
 #get data
-load("~/R/osstat/data/os_ind_visits+actions_2017-10-02_2018-03-31.RData")
+fdata <- "~/R/osstat/data/os_ind_visits+actions_2017-10-02_2018-03-31.RData"
+load(fdata)
+rm(fdata)
 
 #create table of requested pages
-pr <- a[a$field %in% c("url","idpageview","pageIdAction"),]
+require(tidyr, dplyr)
+pr <- a[a$field %in% c("url","pageIdAction"),names(a)[!names(a) %in% c("date")]]
 pr[pr$field=="url",]$value <- sub("https://www.euro-area-statistics.org","",pr[pr$field=="url",]$value)
+pr <- spread(pr, field, value)
+pr <- pr[,names(pr)[!names(pr) %in% c("idVisit","step")]]
+pr <- pr %>% group_by(url) %>% mutate(n=n())
+pr <- unique(pr)
+pr <- pr[with(pr,order(-n)),]
+
+#split the url:
+pr <- pr %>% mutate(page=lapply(strsplit(url,"\\?"),`[`,1))
+pr <- pr %>% mutate(args=lapply(strsplit(url,"\\?"),`[`,2))
+head(lapply(x, `[`, 2))
