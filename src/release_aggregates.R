204641ec1e5307959fa631c06709502fd81dcf59
Laurent <laurent.f.camus@gmail.com>
2018-04-14 23:18:50 +0200

    .

--- src/release_aggregates.R
+++ src/release_aggregates.R
@@ -16,20 +16,20 @@ invisible(lapply(va,function(x){
   ag[[y[1]]][[y[2]]][,y[[3]]] <<- NULL
   }))
 print(paste(length(va),"useless variables dropped"))
-rm(va,to.drop)
+rm(to.drop)
 
 #convert numeric variables coded in string to numeric:
 to.num <- c()
 to.match <- c("nb_","max_","sum_","avg_","min_","_count(_|$)","value")
 to.exclude <- "nb_visits_percentage"
 
-l <- lapply(names(ag),function(x)
+invisible(lapply(names(ag),function(x)
   lapply(names(ag[[x]]),function(y)
     lapply(names(ag[[x]][[y]]),function(z) {
       s <- sapply(to.match,function(r) grep(r,z))
       to.num <<- c(to.num,paste(x,y,z,s))
     }
-    )))
+    ))))
 
 to.num <- to.num[grep("\\s1$",to.num)]
 to.exclude <- grep(paste0(to.exclude,"\\s1$"),to.num)
@@ -37,20 +37,24 @@ to.num <- to.num[-to.exclude]
 to.num <- gsub("\\s1$","",to.num)
 to.num <- strsplit(to.num," ")
 
-l <- lapply(to.num,function(x)
+invisible(lapply(to.num,function(x)
   ag[[x[1]]][[x[2]]][,x[3]] <<- tryCatch({
     as.numeric(ag[[x[1]]][[x[2]]][,x[3]])
   }, warning=function(w) {
     print(w$message)
     print(paste(x[1],x[2],x[3]))
-  }))
+  })))
+
+#convert to numeric percentage variables
+invisible(lapply(strsplit(va[grep("rate",va)],"\\."),function(x)
+  ag[[x[1]]][[x[2]]][,x[3]] <<- as.numeric(sub("%","",ag[[x[1]]][[x[2]]][,x[3]]))))
 
 #export data
 f <- paste0("data/os-aggregates_",min(as.Date(ag[[1]][[1]]$date)),"-",max(as.Date(ag[[1]][[1]]$date)),".RData")
 save(ag,file=f)
 print(paste("updated aggregates released:",f))
 
-rm(l,to.exclude,to.match,to.num,f,ag)
+rm(va,to.exclude,to.match,to.num,f,ag)
 
 #end
 
