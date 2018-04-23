1c45215ad48a5b208c6e20c05bc4d64684c0f7e5
Laurent <laurent.f.camus@gmail.com>
2018-04-23 11:27:45 +0200

    .

--- src/transition_page.R
+++ src/transition_page.R
@@ -11,8 +11,12 @@ a <- a %>% group_by(idVisit) %>% mutate(prev.a=ifelse(row_number()==1,NA,prev.a)
 a <- a %>% group_by(idVisit) %>% mutate(next.a=ifelse(row_number()==n(),NA,next.a))
 
 apply(a,1,function(x) {
-  val.next <- ifelse(is.na(as.character(x$next.a)),"END",as.character(x$next.a))
-  m[as.character(x$pageIdAction),val.next] <<- m[as.character(x$pageIdAction),val.next]+1
+  val.next <- x["next.a"]
+  # val.next <- ifelse(val.next=="NA","END",val.next)
+  val.next <- ifelse(is.na(val.next),"END",val.next)
+  val.next <- gsub(" ","",val.next)
+  pia <- gsub(" ","",x["pageIdAction"])
+  m[pia,val.next] <<- m[pia,val.next]+1
 })
 
   
