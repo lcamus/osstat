d33a59c248d27b35081bdb04d9582929d54982b4
Laurent <laurent.f.camus@gmail.com>
2018-04-23 10:57:10 +0200

    .

--- src/transition_page.R
+++ src/transition_page.R
@@ -1,8 +1,8 @@
 #matrix transition pageIdAction
 
 suppressPackageStartupMessages(require(dplyr))
-dn <- c(sort(pr$pageIdAction),NA)
-m <- matrix(nrow=nrow(pr)+1,ncol=nrow(pr)+1,dimnames=list(dn,dn))
+dn <- sort(pr$pageIdAction)
+m <- matrix(0,nrow=nrow(pr)+1,ncol=nrow(pr)+1,dimnames=list(c(dn,"BEGIN"),c(dn,"END")))
 rm(dn)
 a$prev.a <- lag(a$pageIdAction)
 a$next.a <- lead(a$pageIdAction)
@@ -10,4 +10,9 @@ a$next.a <- lead(a$pageIdAction)
 a <- a %>% group_by(idVisit) %>% mutate(prev.a=ifelse(row_number()==1,NA,prev.a))
 a <- a %>% group_by(idVisit) %>% mutate(next.a=ifelse(row_number()==n(),NA,next.a))
 
-lapply(a,function(x) m[x$pageIdAction,x$next.a] <<- m[x$pageIdAction,x$next.a]+1)
+apply(a,1,function(x) {
+  val.next <- ifelse(is.na(as.character(x$next.a)),"END",as.character(x$next.a))
+  m[as.character(x$pageIdAction),val.next] <<- m[as.character(x$pageIdAction),val.next]+1
+})
+
+  
